86d7ffca437de61fe0c21be20726057ad80d963d
Josep Vidal <josep2@gmail.com>
2018-11-04 09:55:33 +0100

    test3

---
+++ build/CMakeFiles/circle_detector.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/josep/Dropbox/00.Màster Robòtica 2018-2019/02.Sistemes de Percepció/Exercicis/webcam_circles/src/circle_detector.cpp" "/home/josep/Dropbox/00.Màster Robòtica 2018-2019/02.Sistemes de Percepció/Exercicis/webcam_circles/build/CMakeFiles/circle_detector.dir/src/circle_detector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
